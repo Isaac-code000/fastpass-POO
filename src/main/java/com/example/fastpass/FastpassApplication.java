fb90b23e24eb70fd8266aa5e621561435c300392
Isaac-code000 <isaac.cade26@gmail.com>
2026-07-31 15:49:02 -0300

    Configurando o spring pra usar as portas do banco de dados

---
+++ src/main/java/com/example/fastpass/FastpassApplication.java
@@ -0,0 +1,11 @@
+package com.example.fastpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastpassApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FastpassApplication.class, args);
+	}
+}
